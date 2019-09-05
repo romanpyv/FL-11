699048eb6d43eac6232c4e23f42c3490f76daadd
Roman Pyvovar <51457803+romanpyv@users.noreply.github.com>
2019-09-05 10:57:51 +0300

    Add files via upload

---
+++ FL11_HW20_homework_react/homework/emoji-shop/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from "react-dom";
+import {Shop} from "./components/shop/shop";
+import './index.css';
+
+ReactDOM.render(
+    <Shop/>,
+    document.getElementById('root')
+);
